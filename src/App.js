a544c35c76ed2c81d272a5832ad98e04ebda2512
Edvins Antonovs <edvins.antonovs@the-contactgroup.com>
2016-12-20 08:01:21 +0000

    Basic test to see if react-bootstrap works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,6 +13,8 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+
+          <Button bsStyle="success">Success</Button>
         </p>
       </div>
     );
