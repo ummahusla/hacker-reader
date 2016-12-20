643459d57e0ef58c0c3e2137dac8167aa81ab11f
Edvins Antonovs <ummahusla@gmail.com>
2016-12-20 23:04:52 +0000

    WIP. Fix the error in description
    Feed.js:11 Uncaught (in promise) TypeError: Cannot read property
    'setState' of undefined

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import Navigation from '../navigation/Navigation';
 import './Feed.css';
 
+import * as api from '../../helpers/api';
+
+api.getTopStories()
+  .then((response) => {
+    if(response.data) {
+       var topStoryIds = response.data;
+       this.setState({ topStoryIds: topStoryIds });
+    }
+    console.log(response.data);
+});
+
 class Feed extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      topStoryIds: []
+    };
+  }
+
   render() {
     return (
       <div>
 
         <Navigation />
+
+         <div>
+           <pre>{this.state.topStoryIds}</pre>
+         </div>
 
       </div>
     );
