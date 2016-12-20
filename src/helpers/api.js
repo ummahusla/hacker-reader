835ce7988598678c0c7f1002138ce1758deff82e
Edvins Antonovs <ummahusla@gmail.com>
2016-12-20 20:41:11 +0000

    :rocket: Added 3 basic api calls

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,1 +1,13 @@
 import axios from 'axios';
+
+export function getTopStories() {
+    return axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+}
+
+export function getNewStories() {
+    return axios.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty');
+}
+
+export function getBestStories() {
+    return axios.get('https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty');
+}
