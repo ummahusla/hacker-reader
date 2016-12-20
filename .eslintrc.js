49de94464adc6d76468345bfb43e553d7393c220
Edvins Antonovs <ummahusla@gmail.com>
2016-12-20 22:32:00 +0000

    Updated ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "installedESLint": true,
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
+  "parser": "babel-eslint",
+  "plugins": [
+    "react",
+    "flowtype"
+  ],
+  "env": {
+    "commonjs": true,
+    "mocha": true,
+    "es6": true,
+    "node": true,
+    "jquery": true
+  },
+  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:flowtype/recommended"],
+  "parserOptions": {
+    "sourceType":"module",
+    "ecmaVersion": 7,
+    "ecmaFeatures": {
+      "jsx": true,
+      "modules": true,
+      "experimentalObjectRestSpread": true
     }
-};
+  },
+  "rules": {
+    "react/prefer-stateless-function": 1,
+    "camelcase": [2, {"properties": "always"}],
+    "semi": [2, "always"],
+    "no-multiple-empty-lines": [2, {"max": 1}]
+  }
+}
