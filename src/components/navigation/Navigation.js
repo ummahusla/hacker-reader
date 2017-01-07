68a5b57423247b6a248f0be94661547fb7f3326a
Edvins Antonovs <ummahusla@gmail.com>
2017-01-07 20:37:40 +0000

    Navigation class is now a pure function

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Grid, Navbar } from 'react-bootstrap';
 import './Navigation.css';
 
-class Navigation extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar inverse fixedTop>
-            <Grid>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href="/">HackerNews Reader</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-            </Grid>
-        </Navbar>
-      </div>
-    );
-  }
-}
+const Navigation = () => (
+  <div>
+    <Navbar inverse fixedTop>
+        <Grid>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="/">HackerNews Reader</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+        </Grid>
+    </Navbar>
+  </div>
+);
 
 export default Navigation;
