69408c6c421d83cfdc3634464fa2702cd5d01d4f
Edvins Antonovs <ummahusla@gmail.com>
2017-01-07 21:35:18 +0000

    Added getStoryItem api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
 export function getTopStories() {
-    return axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+  return axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
 }
 
 export function getNewStories() {
-    return axios.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty');
+  return axios.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty');
 }
 
 export function getBestStories() {
-    return axios.get('https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty');
+  return axios.get('https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty');
+}
+
+  return axios.get('https://hacker-news.firebaseio.com/v0/item/' + storyId + '.json?print=pretty');
 }
