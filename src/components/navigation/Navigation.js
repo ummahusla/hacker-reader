46aa22fedcc77cde58866474213b17ea0bbf9c40
Edvins Antonovs <ummahusla@gmail.com>
2017-01-17 21:43:01 +0200

    Removed unused variables

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
-import { LinkContainer } from 'react-router-bootstrap';
-import { Button, Grid, Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Grid, Navbar, Nav, NavItem } from 'react-bootstrap';
 import './Navigation.css';
 
 const Navigation = () => (
