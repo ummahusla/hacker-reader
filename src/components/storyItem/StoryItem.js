120b544024765194307af152e64cc19441dd4876
Edvins Antonovs <ummahusla@gmail.com>
2017-01-30 22:59:44 +0000

    Created CommentsLabel component

--- src/components/storyItem/StoryItem.js
+++ src/components/storyItem/StoryItem.js
@@ -32,19 +32,21 @@ class StoryItem extends Component {
     const storyItem = this.state.storyItems;
     const time = new Date(storyItem.time * 1000);
 
-    let comments;
-
-    if (typeof storyItem.kids !== 'undefined') {
-      comments = storyItem.kids.length + ' comments';
-    } else {
-      comments = 'No comments yet';
-    }
+    const CommentsLabel = (props) => {
+      if (typeof props.comments !== 'undefined' && props.comments.length !== 1) {
+        return <Label bsStyle="success">{props.comments.length} comments</Label>;
+      } else if (typeof props.comments !== 'undefined' && props.comments.length === 1) {
+        return <Label bsStyle="success">{props.comments.length} comment</Label>;
+      } else {
+        return <Label>No comments</Label>;
+      }
+    };
 
     return (
       <div>
         <ul className="storyitem-list">
           <li>
-            <i className="fa fa-caret-square-o-up" aria-hidden="true"></i> {storyItem.score}<Button href={storyItem.url} bsStyle="link">{storyItem.title}</Button> <Label>{storyItem.by}</Label> <Label>{moment(time).fromNow()}</Label> <Label>{comments}</Label>
+            <i className="fa fa-caret-square-o-up" aria-hidden="true"></i> {storyItem.score}<Button href={storyItem.url} bsStyle="link">{storyItem.title}</Button> <Label>{storyItem.by}</Label> <Label>{moment(time).fromNow()}</Label> <CommentsLabel comments={storyItem.kids}/>
           </li>
         </ul>
       </div>
