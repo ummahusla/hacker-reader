ae3f8df781788864b6f830f0e7eca1b524db6f38
Edvins Antonovs <ummahusla@gmail.com>
2017-02-01 07:44:18 +0000

    :fire: Fixed bug with navigation
    Added Navbar.Toggle and Navbar.Collapse elements as per
    `react-bootstrap` docs.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -10,28 +10,31 @@ const Navigation = () => (
         <Navbar.Brand>
           <i className="fa fa-hacker-news fa-lg" aria-hidden="true"></i> Hacker News Reader
         </Navbar.Brand>
+        <Navbar.Toggle />
       </Navbar.Header>
-      <Nav>
-        <LinkContainer to="/top">
-          <NavItem eventKey={1}>Top</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/new">
-          <NavItem eventKey={2}>New</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/best">
-          <NavItem eventKey={3}>Best</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/jobs">
-          <NavItem eventKey={4}>Jobs</NavItem>
-        </LinkContainer>
-        <NavDropdown eventKey={5} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={5.1}>Action</MenuItem>
-          <MenuItem eventKey={5.2}>Another action</MenuItem>
-          <MenuItem eventKey={5.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={5.3}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
+      <Navbar.Collapse>
+        <Nav>
+          <LinkContainer to="/top">
+            <NavItem eventKey={1}>Top</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/new">
+            <NavItem eventKey={2}>New</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/best">
+            <NavItem eventKey={3}>Best</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/jobs">
+            <NavItem eventKey={4}>Jobs</NavItem>
+          </LinkContainer>
+          <NavDropdown eventKey={5} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={5.1}>Action</MenuItem>
+            <MenuItem eventKey={5.2}>Another action</MenuItem>
+            <MenuItem eventKey={5.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={5.3}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   </div>
 );
